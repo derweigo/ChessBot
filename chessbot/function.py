ab7012182fedd188671f99058d23211b0ad06a42
Jonathan Skopp <arvecx@gmail.com>
2021-09-22 21:31:04 +0200

    Add Live Game

--- chessbot/function.py
+++ chessbot/function.py
@@ -1,5 +1,15 @@
 from lichesspy.api import user as liuser
 
+
 def get_elo(user: str, timecontrol: str) -> int:
     """ Return the lichess Rating  """
     return liuser(user)['perfs'][timecontrol]['rating']
+
+
+def live_game(user: str) -> str:
+    """ Return the live game played by the user """
+    try:
+        game = liuser.api.user(user)['playing']
+    except:
+        game = "This user is not playing a game right now."
+    return game
--- chessbot/main.py
+++ chessbot/main.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 from config import TOKEN
-from function import get_elo
+from function import *
 
 # Set command prefix
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
@@ -13,8 +13,9 @@ async def get_rapid(ctx, user):
     try:
         await ctx.send(f"{user}s rapid rating on Lichess is ", get_elo(user, "rapid"))
     except Exception as error:
-        await ctx.send("Leider ist ein Fehler aufgetreten.")
-        await ctx.send(f"Fehler: {error}")
+        await ctx.send("Unfortunately an error has occurred")
+        await ctx.send(f"Error:: {error}")
+
 
 @bot.command(name='blitz')
 async def get_blitz(ctx, user):
@@ -22,8 +23,9 @@ async def get_blitz(ctx, user):
     try:
         await ctx.send(f"{user}s rapid rating on Lichess is ", get_elo(user, "blitz"))
     except Exception as error:
-        await ctx.send("Leider ist ein Fehler aufgetreten.")
-        await ctx.send(f"Fehler: {error}")
+        await ctx.send("Unfortunately an error has occurred")
+        await ctx.send(f"Error:: {error}")
+
 
 @bot.command(name='bullet')
 async def get_bullet(ctx, user):
@@ -31,8 +33,17 @@ async def get_bullet(ctx, user):
     try:
         await ctx.send(f"{user}s rapid rating on Lichess is ", get_elo(user, "bullet"))
     except Exception as error:
-        await ctx.send("Leider ist ein Fehler aufgetreten.")
-        await ctx.send(f"Fehler: {error}")
+        await ctx.send("Unfortunately an error has occurred")
+        await ctx.send(f"Error:: {error}")
 
+
+@bot.command(name='live')
+async def get_live_game(ctx, user):
+    """ The function returns the last gmae.  """
+    try:
+        await ctx.send(f"Here you can find the last game of {user}:", live_game(user))
+    except Exception as error:
+        await ctx.send("Unfortunately an error has occurred")
+        await ctx.send(f"Error:: {error}")
 
 bot.run(TOKEN)
