2337d55267d94f419a437db643bdf382f77e71da
Arvecx <arvecx@gmail.com>
2021-09-22 11:09:30 +0200

    Hotfix

--- chessbot/main.py
+++ chessbot/main.py
@@ -1,8 +1,6 @@
-import urllib
-import json
+import lichesspy.api as lichess
 from discord.ext import commands
 from config import TOKEN
-import lichesspy.api as lichess
 
 # Set command prefix
 bot = commands.Bot(command_prefix='!')
@@ -10,14 +8,18 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='elo')
 async def get_elo(ctx, arg1, arg2):
-   """ 
+    """ 
         The function returns the Lichess rating of the searched player.
     """
+    try:
 
-   # Loads all information of the user into the variable
-   user = lichess.user(arg1)['perfs'][arg2]['rating']
-   
-   # Sends the message to the channel
-   await ctx.send(f"{arg1}s {arg2} rating auf Lichess ist: {user}")
+        # Loads all information of the user into the variable
+        user = lichess.user(arg1)['perfs'][arg2]['rating']
+
+        # Sends the message to the channel
+        await ctx.send(f"{arg1}s {arg2} rating auf Lichess ist: {user}")
+    except Exception as e:
+        await ctx.send("Leider ist ein Fehler aufgetreten.")
+        await ctx.send(f"Fehler: {e}")
 
 bot.run(TOKEN)
