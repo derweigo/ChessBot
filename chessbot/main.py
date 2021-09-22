324b4144561683a20d319da62c531dbdf5987c5c
Jonathan Skopp <zeyecx@gmail.com>
2021-09-22 12:41:05 +0200

    Change the prefix.
    Change the prefix to comply with Discord standards.

--- chessbot/main.py
+++ chessbot/main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from config import TOKEN
 
 # Set command prefix
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='/')
 
 
 @bot.command(name='elo')
